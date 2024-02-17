b4b2ca55500a19e744b7de571ef278634872c06b
dinhtuananh <dinhtuananh410@gmail.com>
2024-02-17 12:01:52 +0700

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./src/**/*.{html,js}"],
   theme: {
     extend: {
       fontFamily: {
@@ -10,8 +10,13 @@ module.exports = {
         main: "#252525",
       },
       backgroundImage: {
-        linear_orange: "linear-gradient(180deg,#FFB469,#E57709)",
-        linear_blue: "linear-gradient(159deg,#4EBEFF,#0679BB)",
+        linear_orange: "linear-gradient(180deg, #FFB469 0%, #E57709 100%)",
+        linear_blue: "linear-gradient(135deg, #4EBEFF 0%, #0679BB 100%)",
+        linear_green: "linear-gradient(135deg, #2891CC 0%, #0A9949 100%)",
+      },
+      boxShadow: {
+        custom: "0px 8px 40px rgba(0, 0, 0, 0.15)",
+        custom2: "0px 10px 24px rgba(40, 145, 204, 0.5)",
       },
     },
   },
