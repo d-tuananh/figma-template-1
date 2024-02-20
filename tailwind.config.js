654872b679a49473c2186a248b913419115822e5
dinhtuananh <dinhtuananh410@gmail.com>
2024-02-20 16:34:18 +0700

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,6 +18,7 @@ module.exports = {
       boxShadow: {
         custom: "0px 8px 40px rgba(0, 0, 0, 0.15)",
         custom2: "0px 10px 24px rgba(40, 145, 204, 0.5)",
+        custom3: "0px 0px 40px rgba(0, 0, 0, 0.08)",
       },
     },
   },
