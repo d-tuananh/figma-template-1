29b3e3c12c17f488e8e5c062e073e8384584ad3a
dinhtuananh <dinhtuananh410@gmail.com>
2024-02-21 10:37:25 +0700

    up

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,6 +19,7 @@ module.exports = {
         custom: "0px 8px 40px rgba(0, 0, 0, 0.15)",
         custom2: "0px 10px 24px rgba(40, 145, 204, 0.5)",
         custom3: "0px 0px 40px rgba(0, 0, 0, 0.08)",
+        "ct-orange": "0px 8px 16px 0px rgba(229, 119, 9, 0.35)",
       },
     },
   },
